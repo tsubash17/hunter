8054b699f859f40b92be0aa981294f3c85aaad84
tsubash17 <36472076+tsubash17@users.noreply.github.com>
2018-05-24 11:57:50 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=[]
+for x in range(0,a):
+    c=input()
+    b.append(c)
+d=[]
+e=[]
+for x in b:
+    if x not in d:
+        d.append(x)
+    else:
+        e.append(x)
+e.sort()
+print (e)
